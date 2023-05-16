06dc4419200843619e01a9bd9e1fee17fc33ee8f
rzdtjb <rzdtjb@gmail.com>
2023-05-16 12:17:55 +0800

    beanutils

---
+++ src/com/it/servlet/reqDemo6.java
@@ -0,0 +1,40 @@
+package com.it.servlet;
+
+import com.it.bean.Student;
+import org.apache.commons.beanutils.BeanUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.beans.PropertyDescriptor;
+import java.beans.beancontext.BeanContext;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+@WebServlet("/reqDemo6")
+public class reqDemo6 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, String[]> map = req.getParameterMap();
+        Student stu = new Student();
+
+        try {
+            BeanUtils.populate(stu,map);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(stu);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
